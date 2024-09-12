f109ae81d375a00383cc7c1217362e66720db39a
Juan Reyes <camiloreyes1125@gmail.com>
2024-09-11 22:41:53 -0400

    User model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { Schema, model, Types } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: String,
+
+    userName: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  },
+
+  {
+    timeseries: true,
+  }
+);
+
+module.exports = model("User", userSchema)
